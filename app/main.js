8498e25aabe464431ad6256ab92837ceeb08f097
Borja <abal.varela@gmail.com>
2026-05-05 12:44:12 +0200

    Add logo aplicacion

--- app/main.js
+++ app/main.js
@@ -15,6 +15,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 900,
     height: 700,
+    icon: path.join(__dirname, 'assets/icon.ico'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
